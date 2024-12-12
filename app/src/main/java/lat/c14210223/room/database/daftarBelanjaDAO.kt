ba0f5e58b15099b3ac40da24da960c43bf786b82
Theodorus Dustin <154519546+c14210223@users.noreply.github.com>
2024-12-13 02:31:11 +0700

    Progress 7-12

--- app/src/main/java/lat/c14210223/room/database/daftarBelanjaDAO.kt
+++ app/src/main/java/lat/c14210223/room/database/daftarBelanjaDAO.kt
@@ -1,4 +1,29 @@
 package lat.c14210223.room.database
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface daftarBelanjaDAO {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(daftar: daftarBelanja)
+
+    @Query("UPDATE daftarBelanja SET tanggal =:isi_tanggal, item =:isi_item, jumlah =:isi_jumlah, status=0 " +
+            "WHERE id=:pilihid")
+    fun update(isi_tanggal:String ,isi_item: String , isi_jumlah: String,
+               pilihid: Int)
+
+    @Query("select * from daftarBelanja Where id=:isi_id")
+    suspend fun getItem(isi_id : Int) : daftarBelanja
+
+
+    @Delete
+    fun delete(daftar:daftarBelanja)
+
+    @Query("SELECT * FROM daftarBelanja Order by id asc")
+    fun selectALL() : MutableList<daftarBelanja>
 }
